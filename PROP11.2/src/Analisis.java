969b0308e5339bfc0d78ff8d2154ab74b1bda09c
micky101224 <micky101224@gmail.com>
2017-03-23 21:02:43 +0100

    organizacion
    Raphael solo subiste al git solo la carpeta source no todo el proyecto.
    He creado un proyecto nuevo "PROP11.2" y he metido tu clase ahi. Si os
    parece bien trabajamos todos sobre esa?

---
+++ PROP11.2/src/Analisis.java
@@ -0,0 +1,72 @@
+package prop11.analyse;
+
+import java.util.List;
+import java.util.ArrayList;
+public class Analisis {
+	
+	private List<String> paths;
+	private Administrador admin;
+	private Encuesta encuesta;
+	
+	public Analisis(List<String> pathsInput, Administrador admin, Encuesta encuesta){
+		paths = new ArrayList<String>();
+		for(String s: pathsInput){
+			paths.add(s); //Strings are imutable so no problem			
+		}
+		
+		this.admin = admin; //Do I need to store admin?
+		this.encuesta = encuesta;
+	}
+	
+	public List<Respuesta_Encuesta> importResp(){
+		
+	}
+	
+	public Resultados k_means(List<Respuesta_Encuesta> listToAnalyse, int k, double thresholdDist){
+		
+		//variable listToAnalyse is an array containing the results we want to analyse
+		//variable k is the number of centroids we want
+		//variable sizeVector is the number of question of the questionnary
+		//variable thresholdDist is the distance that will determine when k-mean will end
+		
+		//CREATION OF SEEDS
+		//generar los seeds random?
+		Respuesta_Encuesta seed = new Respuesta_Encuesta();
+		for(int i = 0; i < k; i++){
+			for(int j = 0; j < encuesta.size() ; j++){
+				//generate random answer "random" to the question ecuesta.get(j).type()
+				Respuesta_Pregunta rp = new Respuesta_Pregunta(random);
+				seed.add(rp);
+			}			
+		}
+		
+		//ASSIG EACH RESPUESTA_ENCUESTA TO CLUSTER
+		for(Respuesta_encuesta ra: listToAnalyse){
+			double distance_min = Double.POSITIVE_INFINITY;
+			int index_centroid = 0;
+			
+			for(int i = 0; i < k; i++){
+				double distance = 0;
+				for(int index = 0; index < encuesta.size(); index++){
+					//Switch encuesta.get(index).type case ...
+					//Compute distance between ra.get(index) and seeds.get(index)
+					//Increment distance
+				}
+				if(distance < distance_min){
+					distance_min = distance;
+					index_centroid = k;
+				}
+			}
+			
+			//Store the result of assignation
+			
+			
+		}
+		
+		
+		
+	}
+	
+	
+	
+}
